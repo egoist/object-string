210c1d017c51174ad77674639108aa1d68d0aa29
EGOIST <0x142857@gmail.com>
2016-01-21 01:22:49 +0800

    lint

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ module.exports = function (str) {
 	if (typeof str !== 'string') {
 		throw new TypeError('Expect a string but got ' + typeof str)
 	}
+	var result = {}
 	// check if str is a valid array-string
 	if (str.indexOf(',') > -1) {
 		var arr = str.split(',')
 		var treatAsObject = arr[0].indexOf(':') > -1
 		if (treatAsObject) {
-			var result = {}
 			arr.forEach(function (item) {
 				var yields = item.split(':')
 				result[yields[0]] = yields[1]
@@ -19,7 +19,6 @@ module.exports = function (str) {
 	// check if str is a valid object-string
 	if (str.indexOf(':') > -1) {
 		var yields = str.split(':')
-		var result = {}
 		result[yields[0]] = yields[1]
 		return result
 	}
